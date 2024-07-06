1e48a2d8313a3732652cd992bd09ac7c262b6203
Drachir <gaming.drachir@gmail.com>
2024-07-06 17:16:04 +0200

    added spam detection

--- cogs/event_listeners.py
+++ cogs/event_listeners.py
@@ -2,35 +2,58 @@ import asyncio
 from datetime import datetime, timedelta, timezone
 import json
 import discord
+import discord_timestamps
 from discord import app_commands
 from discord.ext import commands, tasks
+import cogs.moderation as modcog
+from discord_timestamps import TimestampType
 
 class Listener(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.messages = dict()
+        self.spam_threshold = 6
     
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
         if message.author.id == self.bot.user.id:
             return
         if message.guild:
-            # if message.author.id not in self.messages:
-            #     self.messages[message.author.id] = [message]
-            # else:
-            #     self.messages[message.author.id].append(message)
-            #     if len(self.messages[message.author.id]) > 1:
-            #         date_spam = datetime.now(tz=timezone.utc) - timedelta(seconds=10)
-            #         spam_count = 0
-            #         msg: discord.Message
-            #         for i, msg in enumerate(self.messages[message.author.id][:]):
-            #             if msg.created_at > date_spam:
-            #                 spam_count += 1
-            #                 await message.channel.send("spam")
-            #             else:
-            #                 self.messages[message.author.id].pop(i)
-            #                 continue
-            #             print(self.messages[message.author.id])
+            if message.author.id not in self.messages:
+                self.messages[message.author.id] = [message]
+            else:
+                self.messages[message.author.id].append(message)
+                if len(self.messages[message.author.id]) >= self.spam_threshold:
+                    channels = set()
+                    date_spam = datetime.now(tz=timezone.utc) - timedelta(seconds=20)
+                    spam_count = 0
+                    msg: discord.Message
+                    for i, msg in enumerate(self.messages[message.author.id][:]):
+                        if msg.created_at > date_spam:
+                            channels.add(msg.channel.name)
+                            spam_count += 1
+                        else:
+                            self.messages[message.author.id].pop(i)
+                            continue
+                    if spam_count >= self.spam_threshold and len(channels) >= self.spam_threshold:
+                        await message.author.timeout(timedelta(days=1), reason="Likely spam bot")
+                        output_string = (f"{self.bot.user.mention} detected a likely spam bot:"
+                                         f"\n{message.author.mention} (Muted for 1 day)"
+                                         f"\n**Deleted messages:**")
+                        for msg in self.messages[message.author.id]:
+                            d_timestamp = discord_timestamps.format_timestamp(msg.created_at.timestamp(), TimestampType.RELATIVE)
+                            output_string += f"\nMessage in {msg.channel.name} at {d_timestamp}\n{msg.content}"
+                            await msg.delete()
+                        embed = discord.Embed(color=0xDE1919, description=output_string)
+                        channel_ids = modcog.get_channels(message.guild.id)
+                        modlogs = await self.bot.fetch_channel(channel_ids["mod_logs"])
+                        await modlogs.send(embed=embed)
+                        embed2 = discord.Embed(color=0xDE1919, title=f"You have been muted for spam-bot like behavior.\nDuration: 1 Day")
+                        del self.messages[message.author.id]
+                        try:
+                            await message.author.send(embed=embed2)
+                        except Exception:
+                            pass
             return
         else:
             await message.channel.send("This Bot does not accept messages. For ban appeals please visit https://legiontd2.com/bans")
